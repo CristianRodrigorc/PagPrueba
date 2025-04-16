c0d8155e71f4928e8bc38164b71c6c993450f6c5
CristianRodrigorc <cristian24121997@gmail.com>
2025-04-16 10:48:46 +0200

    corrección del dato enviado al back

--- public/js/ubicaciones.js
+++ public/js/ubicaciones.js
@@ -5,10 +5,14 @@ fetch('js/arbol.json')
     const provinciaSelect = document.getElementById('provincia');
     const municipioSelect = document.getElementById('municipio');
 
+    let selectedComunidad = '';
+    let selectedProvincia = '';
+    let selectedMunicipio = '';
+
     // Cargar las Comunidades Autónomas
     data.forEach(comunidad => {
       const option = document.createElement('option');
-      option.value = comunidad.code;  // Usamos el código para filtrar, pero enviamos el nombre
+      option.value = comunidad.code; // Usamos el código para filtrar, pero el valor será el nombre
       option.textContent = comunidad.label;
       comunidadSelect.appendChild(option);
     });
@@ -19,9 +23,10 @@ fetch('js/arbol.json')
       municipioSelect.innerHTML = '<option value="">Seleccione un municipio</option>';
       const comunidad = data.find(c => c.code === comunidadSelect.value);
       if (comunidad) {
+        selectedComunidad = comunidad.label;  // Guardamos el nombre de la comunidad
         comunidad.provinces.forEach(provincia => {
           const option = document.createElement('option');
-          option.value = provincia.code;  // Usamos el código para filtrar, pero enviamos el nombre
+          option.value = provincia.code;  // Usamos el código para filtrar, pero el valor será el nombre
           option.textContent = provincia.label;
           provinciaSelect.appendChild(option);
         });
@@ -35,12 +40,27 @@ fetch('js/arbol.json')
       if (comunidad) {
         const provincia = comunidad.provinces.find(p => p.code === provinciaSelect.value);
         if (provincia) {
+          selectedProvincia = provincia.label;  // Guardamos el nombre de la provincia
           provincia.towns.forEach(municipio => {
             const option = document.createElement('option');
-            option.value = municipio.code;  // Usamos el código para filtrar, pero enviamos el nombre
+            option.value = municipio.code;  // Usamos el código para filtrar, pero el valor será el nombre
             option.textContent = municipio.label;
             municipioSelect.appendChild(option);
           });
+        }
+      }
+    });
+
+    // Cambiar Municipio seleccionado
+    municipioSelect.addEventListener('change', () => {
+      const comunidad = data.find(c => c.code === comunidadSelect.value);
+      if (comunidad) {
+        const provincia = comunidad.provinces.find(p => p.code === provinciaSelect.value);
+        if (provincia) {
+          const municipio = provincia.towns.find(t => t.code === municipioSelect.value);
+          if (municipio) {
+            selectedMunicipio = municipio.label;  // Guardamos el nombre del municipio
+          }
         }
       }
     });
