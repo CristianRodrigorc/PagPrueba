f0b46077b528a828b7d35eac24d49cdb90228ed8
CristianRodrigorc <cristian24121997@gmail.com>
2025-04-29 10:21:58 +0200

    corrigiendo errores de llamado a la api

--- public/js/script_chatbot.js
+++ public/js/script_chatbot.js
@@ -1,7 +1,7 @@
 let opciones = [];
 
 async function cargarOpciones() {
-  const res = await fetch('opciones.json');
+  const res = await fetch('/api/OcionesChatbot');
   opciones = await res.json();
 
   const contenedor = document.getElementById('options');
